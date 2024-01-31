7527c01fa0ca29b740f9208084a25ba75ececea5
Soumendra Ganguly <soumendraganguly@gmail.com>
2024-02-01 00:44:38 +0100

    Initial commit
    Signed-off-by: Soumendra Ganguly <soumendraganguly@gmail.com>

---
+++ stty.py
@@ -0,0 +1,369 @@
+# Author: Soumendra Ganguly.
+
+import termios
+import copy
+
+__all__ = [
+    "Stty", "NOW", "DRAIN", "FLUSH", "SPEEDS",
+    "IFBOOL_A", "OFBOOL_A", "CFBOOL_A", "LFBOOL_A",
+    "CS_A", "DLY_A", "SPEED_A", "WINSZ_A", "CC_A"
+]
+
+# Indices for termios attribute list.
+_IFLAG = 0
+_OFLAG = 1
+_CFLAG = 2
+_LFLAG = 3
+_ISPEED = 4
+_OSPEED = 5
+_CC = 6
+
+# Indices for termios winsize tuple.
+_ROWS = 0
+_COLS = 1
+
+# Do we have termios.tcgetwinsize() and termios.tcsetwinsize()?
+_HAVE_WINSZ = (hasattr(termios, "TIOCGWINSZ")
+               and hasattr(termios, "TIOCSWINSZ")
+               and hasattr(termios, "tcgetwinsize")
+               and hasattr(termios, "tcsetwinsize"))
+
+# All possible iflag boolean mask names.
+_ifbool = [
+    "IGNBRK", "BRKINT", "IGNPAR", "PARMRK", "INPCK", "ISTRIP",
+    "INLCR", "IGNCR", "ICRNL", "IUCLC", "IXON", "IXANY",
+    "IXOFF", "IMAXBEL"
+]
+
+# All possible oflag boolean mask names.
+_ofbool = [
+    "OPOST", "OLCUC", "ONLCR", "OCRNL",
+    "ONOCR", "ONLRET", "OFILL", "OFDEL"
+]
+
+# All possible cflag boolean mask names.
+_cfbool = [
+    "CSTOPB", "CREAD", "PARENB", "PARODD",
+    "HUPCL", "CLOCAL", "CIBAUD", "CRTSCTS"
+]
+
+# All possible lflag boolean mask names.
+_lfbool = [
+    "ISIG", "ICANON", "XCASE", "ECHO", "ECHOE", "ECHOK",
+    "ECHONL", "ECHOCTL", "ECHOPRT", "ECHOKE", "FLUSHO",
+    "NOFLSH", "TOSTOP", "PENDIN", "IEXTEN"
+]
+
+# Character size mask name (part of cflag)
+# and names of all possible values.
+_cs = {"CSIZE": ["CS5", "CS6", "CS7", "CS8"]}
+
+# All possible delay mask names (parts of oflag)
+# and names of all of their possible values.
+_dly = {
+    "CRDLY": ["CR0", "CR1", "CR2", "CR3"],
+    "NLDLY": ["NL0", "NL1"],
+    "TABDLY": ["TAB0", "TAB1", "TAB2", "TAB3"],
+    "BSDLY": ["BS0", "BS1"],
+    "FFDLY": ["FF0", "FF1"],
+    "VTDLY": ["VT0", "VT1"]
+}
+
+# Names of all possible indices
+# of the control character list.
+_cc = [
+    "VEOF", "VEOL", "VEOL2", "VERASE", "VERASE2", "VWERASE",
+    "VKILL", "VREPRINT", "VINTR", "VQUIT", "VSUSP", "VDSUSP",
+    "VSTART", "VSTOP", "VLNEXT", "VSTATUS", "VDISCARD", "VSWTCH"
+]
+
+# All possible Baud rate "indices".
+_r = [
+    0, 50, 75, 110, 134, 150, 200, 300, 600,
+    1200, 1800, 2400, 4800, 9600, 19200, 38400,
+    57600, 115200, 230400, 460800, 500000, 576000,
+    921600, 1000000, 1152000, 1500000, 2000000,
+    2500000, 3000000, 3500000, 4000000
+]
+
+# Keys of _bool_d are lowercase names of masks that take boolean values
+# (for example one can turn ECHO on/off).
+#
+# Example element of _bool_d.items() is ("echo", (_LFLAG, termios.ECHO))
+_bool_d = {}
+for flag, masklist in [(_IFLAG, _ifbool),
+                       (_OFLAG, _ofbool),
+                       (_CFLAG, _cfbool),
+                       (_LFLAG, _lfbool)]:
+    for mask in masklist:
+        if hasattr(termios, mask):
+            _bool_d[mask.lower()] = (flag, getattr(termios, mask))
+
+# Keys of _num_d are lowercase names for masks that take
+# nonnegative integer values. For example, TABDLY can
+# take the integer value TAB0. However, in this module
+# we will specify the values of such masks as lowercase
+# standard names of the values, for example "tab0" instead
+# of the integer value TAB0.
+#
+# Example element of _num_d.items() is
+# ("nldly", 4-element-tuple), where 4-element-tuple is
+# (_OFLAG, termios.NLDLY,
+# {"nl0": termios.NL0, "nl1": termios.NL1},
+# {termios.NL0: "nl0", termios.NL1: "nl1"})
+_num_d = {}
+for flag, maskdict in [(_CFLAG, _cs), (_OFLAG, _dly)]:
+    for mask, maskvalues in maskdict.items():
+        if hasattr(termios, mask):
+            # Values of mask that are available on
+            # system; for example, if mask is CRDLY
+            # and system only has CR0, CR1, then
+            # avail = {"cr0": termios.CR0, "cr1": termios.CR1}.
+            avail = {v.lower(): getattr(termios, v) for v in
+                                maskvalues if hasattr(termios, v)}
+            # Mapping that is inverse of 'avail'.
+            inverse = {v: k for k, v in avail.items()}
+
+            _num_d[mask.lower()] = (flag, getattr(termios, mask),
+                                       avail, inverse)
+
+# Example element of _baud_d.items() is (50, termios.B50)
+# Example element of _baud_d_inverse.items() is (termios.B50, 50)
+_baud_d = {}
+for n in _r:
+    rate = f"B{n}"
+    if hasattr(termios, rate):
+        _baud_d[n] = getattr(termios, rate)
+
+_baud_d_inverse = {v: k for k, v in _baud_d.items()}
+
+# Example element of _cc_d.items() is ("eof", termios.VEOF)
+_cc_d = {}
+for s in _cc:
+    if hasattr(termios, s):
+        _cc_d[s[1:].lower()] = getattr(termios, s)
+
+_noncanon_d = {"min": termios.VMIN, "time": termios.VTIME}
+
+_speed_d = {"ispeed": _ISPEED, "ospeed": _OSPEED}
+
+_winsz_d = {"rows": _ROWS, "cols": _COLS} if _HAVE_WINSZ else {}
+
+# List of lowercase names of all Stty object
+# attributes available on system (strings).
+_available = [
+    *_bool_d, *_num_d, *_speed_d,
+    *_cc_d, *_noncanon_d, *_winsz_d
+]
+
+# The "action" constants for tcsetattr().
+NOW = termios.TCSANOW
+DRAIN = termios.TCSADRAIN
+FLUSH = termios.TCSAFLUSH
+
+IFBOOL_A = [mask.lower() for mask in _ifbool if hasattr(termios, mask)]
+OFBOOL_A = [mask.lower() for mask in _ofbool if hasattr(termios, mask)]
+CFBOOL_A = [mask.lower() for mask in _cfbool if hasattr(termios, mask)]
+LFBOOL_A = [mask.lower() for mask in _lfbool if hasattr(termios, mask)]
+
+CS_A = {mask.lower(): [v.lower() for v in _cs[mask] if hasattr(termios, v)]
+        for mask in _cs if hasattr(termios, mask)},
+DLY_A = {mask.lower(): [v.lower() for v in _dly[mask] if hasattr(termios, v)]
+         for mask in _dly if hasattr(termios, mask)}
+
+CC_A = [*_cc_d]
+NONCANON_A = [*_noncanon_d]
+SPEED_A = [*_speed_d]
+WINSZ_A = [*_winsz_d]
+
+SPEEDS = [*_baud_d]
+
+class Stty(object):
+    """Manipulate termios and winsize in the style of stty(1)."""
+    def __init__(self, fd):
+        super().__setattr__("_termios", termios.tcgetattr(fd))
+        if _HAVE_WINSZ:
+            super().__setattr__("_winsize", termios.tcgetwinsize(fd))
+        else:
+            super().__setattr__("_winsize", None)
+
+        for name in _bool_d:
+            x = _bool_d[name]
+            value = True if (self._termios[x[0]] & x[1]) else False
+            self.__setattr__(name, value)
+
+        for name in _num_d:
+            x = _num_d[name]
+            y = self._termios[x[0]] & x[1]
+            self.__setattr__(name, x[3][y])
+
+        for name in _speed_d:
+            x = self._termios[_speed_d[name]]
+            self.__setattr__(name, _baud_d_inverse[x])
+
+        for name in _cc_d:
+            self.__setattr__(name, self._termios[_CC][_cc_d[name]])
+
+        for name in _noncanon_d:
+            self.__setattr__(name,
+                             ord(self._termios[_CC][_noncanon_d[name]]))
+
+        if self._winsize:
+            for name in _winsz_d:
+                self.__setattr__(name, self._winsize[_winsz_d[name]])
+
+    def __setattr__(self, name, value):
+        if name == "_termios" or name == "_winsize":
+            raise AttributeError(f"attribute '{name}' must not be "
+                                 "directly modified")
+
+        if name in _bool_d:
+            x = _bool_d[name]
+            if value:
+                self._termios[x[0]] |= x[1]
+            else:
+                self._termios[x[0]] &= ~x[1]
+
+            super().__setattr__(name, value)
+            return
+
+        if name in _num_d:
+            if not isinstance(value, str):
+                raise TypeError(f"value of attribute '{name}' must have "
+                                "type 'str'")
+
+            x = _num_d[name]
+            if value not in x[2]:
+                raise ValueError(f"unsupported value '{value}' for "
+                                 "attribute '{name}'")
+
+            self._termios[x[0]] &= ~x[1]
+            self._termios[x[0]] |= x[2][value]
+
+            super().__setattr__(name, value)
+            return
+
+        if name in _speed_d:
+            if not isinstance(value, int):
+                raise TypeError(f"value of attribute '{name}' must have "
+                                "type 'int'")
+
+            if value not in _baud_d:
+                raise ValueError(f"unsupported value {value} for "
+                                 "attribute '{name}'")
+
+            self._termios[_speed_d[name]] = _baud_d[value]
+
+            super().__setattr__(name, value)
+            return
+
+        if name in _cc_d:
+            if not isinstance(value, bytes):
+                raise TypeError(f"value of attribute '{name}' must have "
+                                "type 'bytes'")
+
+            if len(value) != 1:
+                raise ValueError(f"value of attribute '{name}' must have "
+                                 "length equal to 1")
+
+            self._termios[_CC][_cc_d[name]] = value
+
+            super().__setattr__(name, value)
+            return
+
+        if name in _noncanon_d:
+            if not isinstance(value, int):
+                raise TypeError(f"value of attribute '{name}' must have "
+                                "type 'int'")
+
+            if value < 0:
+                raise ValueError(f"expected Nonnegative value for "
+                                 "attribute '{name}'")
+
+            self._termios[_CC][_noncanon_d[name]] = bytes([value])
+
+            super().__setattr__(name, value)
+            return
+
+        if name in _winsz_d:
+            if not isinstance(value, int):
+                raise TypeError(f"value of attribute '{name}' must have "
+                                "type 'int'")
+
+            if value < 0:
+                raise ValueError(f"expected Nonnegative value for "
+                                 "attribute '{name}'")
+
+            self._winsize[_winsz_d[name]] = value
+
+            super().__setattr__(name, value)
+            return
+
+        raise AttributeError(f"attribute '{name}' unsupported on platform")
+
+    def save(self):
+        """Return deep copy of self."""
+        return copy.deepcopy(self)
+
+    def get(self):
+        """Return dictionary of relevant attributes."""
+        return {x: getattr(self, x) for x in _available}
+
+    def set(self, **opts):
+        """Set multiple attributes as named arguments."""
+        for x in opts:
+            if x not in _available: # or should be some keyword not found error?
+                raise AttributeError(f"attribute '{name}' unsupported on "
+                                     "platform")
+
+        for x in opts:
+            self.__setattr__(x, opts[x])
+
+    def __repr__(self):
+        return ", ".join([f"{x}={getattr(self, x)}" for x in _available])
+
+    def apply(self, fd, when=NOW, apply_termios=True,
+              apply_winsize=True):
+        """Apply settings to terminal."""
+        if apply_termios:
+            termios.tcsetattr(fd, when, self._termios)
+        if apply_winsize and self._winsize:
+            termios.tcsetwinsize(fd, self._winsize)
+
+    def evenp(self, plus=True):
+        """Set/unset evenp combination mode."""
+        if plus:
+            self.set(parenb=True, csize="cs7", parodd=False)
+        else:
+            self.set(parenb=False, csize="cs8")
+
+    def oddp(self, plus=True):
+        """Set/unset oddp combination mode."""
+        if plus:
+            self.set(parenb=True, csize="cs7", parodd=True)
+        else:
+            self.set(parenb=False, csize="cs8")
+
+    def raw(self):
+        """Set raw combination mode."""
+        for x in IFBOOL_A:
+            self.__setattr__(x, False)
+        for x in LFBOOL_A:
+            self.__setattr__(x, False)
+        self.set(opost=False, parenb=False,
+                 csize="cs8", min=1, time=0)
+
+    def nl(self, plus=True):
+        """Set/unset nl combination mode."""
+        if plus:
+            self.icrnl = False
+        else:
+            self.set(icrnl=True, inlcr=False, igncr=False)
+
+    def ek(self):
+        """Set ek combination mode."""
+        if hasattr(termios, "CERASE"):
+            self.erase = termios.CERASE
+
+        if hasattr(termios, "CKILL"):
+            self.kill = termios.CKILL
